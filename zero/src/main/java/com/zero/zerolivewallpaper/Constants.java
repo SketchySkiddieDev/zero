ebf44130474477e49429e67c73845e1d3854485c
SketchySkiddieDev <yay.iam@gmail.com>
2025-02-24 14:54:57 -0600

    Update Constants.java

--- zero/src/main/java/com/zero/zerolivewallpaper/Constants.java
+++ zero/src/main/java/com/zero/zerolivewallpaper/Constants.java
@@ -4,7 +4,7 @@ package com.zero.zerolivewallpaper;
 public class Constants {
 
     // System
-    public final static String URL_API = "https://github.com/lucasasselli/zero/blob/master/wallpapers/";
+    public final static String URL_API = "https://github.com/SketchSkiddieDev/zero/blob/master/wallpapers/";
     public final static String PACKAGE_NAME = "com.zero.zerolivewallpaper";
     public final static String SERVICE_NAME = PACKAGE_NAME + ".wallpaper.MyWallpaperService";
 
